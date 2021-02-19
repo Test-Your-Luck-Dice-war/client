e31b257ead7be9d35ab7df638e21e5a14745058e
Ramadhan Nur Cholis Januar <adhann98@gmail.com>
2021-02-19 15:29:14 +0700

    fixed socket base_url  connection

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -5,7 +5,7 @@ import store from '../store'
 
 const socket = new VueSocketIO({
   debug: true,
-  connection: 'https://test-your-luck-server.herokuapp.com/',
+  connection: 'https://test-your-luck-server.herokuapp.com',
   vuex: {
     store,
     actionPrefix: 'SOCKET_'
