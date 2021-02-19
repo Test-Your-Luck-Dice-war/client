235c0a1c01efef1d8a119e77f41ab66bbce6b63e
kemalyusuke <kemalyusuke@gmail.com>
2021-02-19 14:21:32 +0700

    kurang layouting

--- src/views/battleRoom.vue
+++ src/views/battleRoom.vue
@@ -8,6 +8,10 @@
     <h2>Your Luck : {{diceResult}}</h2>
     <h2 v-if="battleResult">{{battleResult}}</h2>
     <h2 v-if="battleResult">Enemy Luck : {{enemyDice}}</h2>
+    <!-- <button class="btn btn-primary btn-sm" @click.prevent=""><span class="fa fa-play-circle-o"></span></button> -->
+    <!-- <audio controls autoplay class="hidden">
+          <source src="../assets/movie_1.mp3" type="audio/ogg">
+        </audio> -->
   </div>
 </template>
 
@@ -34,6 +38,9 @@ export default {
     },
     enemyName () {
       return this.$store.state.enemyName
+    },
+    enemyLeft () {
+      return this.$store.state.enemyLeft
     }
   },
   methods: {
@@ -43,28 +50,39 @@ export default {
       this.$router.push('/')
     },
     battle () {
+      this.playSound('https://www.myinstants.com/media/sounds/shake-and-roll-dice-soundbible.mp3')
       // console.log(this.$route)
       this.$socket.emit('battle')
       // this.$socket.emit('newBattle')
     },
     compareResult () {
-      if (this.enemy.dice && this.diceResult) {
+      if (this.enemy.dice && this.diceResult && this.enemyName) {
+        this.$store.dispatch('setMatchStatus', false)
         if (this.enemy.dice < this.diceResult) {
+          this.playSound('https://www.myinstants.com/media/sounds/youre-a-winner.mp3')
           Swal.fire(`You win against ${this.enemyName} `)
             .then(() => {
               this.logout()
             })
         } else if (this.enemy.dice > this.diceResult) {
+          this.playSound('https://www.myinstants.com/media/sounds/metal-gear-solid-snake-death-scream-sound-effect_fR1Ryqk.mp3')
           Swal.fire(`You lose against ${this.enemyName}`)
             .then(() => {
               this.logout()
             })
         } else {
           Swal.fire('ITS A DRAW !!')
+          this.playSound('https://www.myinstants.com/media/sounds/ha-gay.mp3')
             .then(() => {
               this.logout()
             })
         }
+      }
+    },
+    playSound (sound) {
+      if (sound) {
+        var audio = new Audio(sound)
+        audio.play()
       }
     }
   },
@@ -74,11 +92,21 @@ export default {
     },
     diceResult (val) {
       this.compareResult()
+    },
+    enemyLeft (val) {
+      if (val) {
+        this.$socket.emit('disconnected')
+      }
     }
   },
   created () {
-    console.log(this.$route.params)
-    this.$store.dispatch('setInGame', true)
+    if (!this.username) {
+      this.$router.push('/')
+    } else {
+      console.log(this.$route.params)
+      this.$store.dispatch('setInGame', true)
+      this.$store.dispatch('setMatchStatus', true)
+    }
   }
 }
 </script>
